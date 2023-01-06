33d096f4296c58192b89d23125ce8bdc8e06a713
Chloe <chloevision97@gmail.com>
2023-01-06 13:29:47 +0100

    INIT

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./modules/Header";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/modules/header/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar(props) {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Resources</li>
+        <li>Booking</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
